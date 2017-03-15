0f0ef12af1301866f5eee9b18a369c2059965d9b
Akash <gupta.akash2020@gmail.com>
2017-03-16 00:10:25 +0530

    category options

--- index.js
+++ index.js
@@ -98,6 +98,7 @@ function processPostback(event){
 
 
 function sendMessage(recipientId, message) {
+    console.log("hi");
     request({
         url: "https://graph.facebook.com/v2.6/me/messages",
         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
