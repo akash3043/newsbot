7b63f7bfd6bac97dc62e4752fcf31bce670c119e
Akash <gupta.akash2020@gmail.com>
2017-03-16 16:25:10 +0530

    asdsa

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ console.log("https://newsapi.org/v1/sources?language=en&apiKey=387b12d8c1e74fde9
               elements[i]={
                 title : sourcesArr[i].name,
                 subtitle : sourcesArr[i].description.substring(0,80),
-                //image_url:sourcesArr[i].urlsToLogos.small,
+                image_url:sourcesArr[i].urlsToLogos.small,
                 buttons: [
                         {
                             "title": "Get top news",
