50360c9828556459034f93415c23aca981cdcd87
Akash <gupta.akash2020@gmail.com>
2017-03-16 17:33:29 +0530

    news articles

--- index.js
+++ index.js
@@ -86,6 +86,9 @@ function processPostback(event){
   }else if(payload==="Sport"){
     getSourceList("sport", senderId)
 
+  }else{
+      console.log(payload);
+      getNewsArticles(payload, senderId);
   }
 }
 
@@ -112,41 +115,94 @@ console.log("https://newsapi.org/v1/sources?language=en&apiKey=387b12d8c1e74fde9
         if(!error&&response.statusCode===200){
           console.log("request passed")
             var sourceObj = JSON.parse(body);
-            var sourcesArr = sourceObj.sources;
-            console.log(sourcesArr);
-            console.log(sourcesArr.length);
-            var elements = new Array(sourcesArr.length);
-            for(var i=0; i<sourcesArr.length;i++){
-              elements[i]={
-                title : sourcesArr[i].name,
-                subtitle : sourcesArr[i].description.substring(0,80),
-                image_url:sourcesArr[i].urlsToLogos.small,
-                buttons: [
-                        {
-                            "title": "Get top news",
-                            "type": "postback",
-                            "payload":sourcesArr[i].id,
+            if(body.status==='ok'){
+                var sourcesArr = sourceObj.sources;
+                console.log(sourcesArr);
+                console.log(sourcesArr.length);
+                var elements = new Array(sourcesArr.length);
+                for(var i=0; i<sourcesArr.length&&i<10;i++){
+                  elements[i]={
+                    title : sourcesArr[i].name,
+                    subtitle : sourcesArr[i].description.substring(0,80),
+                    image_url:sourcesArr[i].urlsToLogos.small,
+                    buttons: [
+                            {
+                                "title": "Get top news",
+                                "type": "postback",
+                                "payload":sourcesArr[i].id,
 
-                        }
-                    ]
-              }
+                            }
+                        ]
+                  }
 
-              console.log(elements);
+                  console.log(elements);
 
 
 
-            }
-            var message = {
-                attachment :{
-                    type:"template",
-                    payload:{
-                        template_type:"generic",
-                        image_aspect_ratio:"square",
-                        elements:elements,
-                    }
                 }
-            };
-              sendMessage(userId, message)
+                var message = {
+                    attachment :{
+                        type:"template",
+                        payload:{
+                            template_type:"generic",
+                            image_aspect_ratio:"square",
+                            elements:elements,
+                        }
+                    }
+                };
+                  sendMessage(userId, message)
+            }else{
+                sendMessage(userId,{text:"Something went wrong. Please try again"})
+            }
+        }else{
+            sendMessage(userId,{text:"Something went wrong. Please try again"})
+        }
+    })
+}
+
+function getNewsArticles(source, userId ){
+    request("https://newsapi.org/v1/articles?sortBy=top&apiKey=387b12d8c1e74fde941fbb27e7764398&source="+source, function(error, response, body){
+        if(!error&&response.statusCode===200){
+            var newsObj = JSON.parse(body);
+            if(newsObj.status==='ok'){
+                var articlesArr = newsObj.articles
+                var elements = new Array(articlesArr.length)
+                for(var i=0; i<articlesArr.length&&i<10;i++){
+                  elements[i]={
+                    title : articlesArr[i].title,
+                    subtitle : articlesArr.description.substring(0,80),
+                    image_url:articlesArr[i].urlsToImage.small,
+                    buttons: [
+                            {
+                                "title": "Read More",
+                                "type": "web_url",
+                                "url":articlesArr[i].url,
+
+                            }
+                        ]
+                  }
+
+
+
+                }
+                var message = {
+                    attachment :{
+                        type:"template",
+                        payload:{
+                            template_type:"generic",
+                            elements:elements,
+                        }
+                    }
+                };
+                  sendMessage(userId, message)
+
+
+
+            }else{
+                sendMessage(userId,{text:"Something went wrong. Please try again"})
+            }
+        }else{
+            sendMessage(userId,{text:"Something went wrong. Please try again"})
         }
     })
 }
