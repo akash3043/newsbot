477ca36037e4dbfc2d12dc76c923bcda3dcdb9a0
Akash <gupta.akash2020@gmail.com>
2017-03-20 13:39:16 +0530

    category implementations

--- index.js
+++ index.js
@@ -713,7 +713,22 @@ function getTechnologyArticles(userId){
       if(err){
         sendMessage(userId, {text:"Something wnet wrong. Please try again"});
       }else{
-          console.log(result);
+          var counter = result.counter;
+          var newsArticles = results.output;
+          if(counter===newsArticles.length){
+            sendMessage(userId, {text:"No more latest articles in this category. Try again later or read about other category?"})
+          }else{
+            var newCounter = counter+10<newsArticles.length?counter+10:newsArticles.length;
+            var outputArr = newsArticles.slice(counter, newCounter);
+            getNewsTemplates(outputArr, userId);
+            UserInput.findOneAndUpdate(query, {counter:newCounter}, {upsert:true}, function(err, result){
+                if(err){
+                    console.log("Database error: ":+ err)
+                }
+            })
+
+          }
+
       }
     })
   }
