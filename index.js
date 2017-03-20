57dd56e826e96cdacb970186459470ce7b5977e0
Akash <gupta.akash2020@gmail.com>
2017-03-20 12:21:34 +0530

    category implementations

--- index.js
+++ index.js
@@ -486,6 +486,143 @@ function getTechnologyArticles(userId){
       })
   }
 
+  function getBusinessArticles(userId){
+
+        async.parallel([
+          function(callback){
+              request("https://newsapi.org/v1/articles?source=bloomberg&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+                if(!error&&response.statusCode){
+                    var responseObj = JSON.parse(body);
+                    var newsArticles = responseObj.articles;
+                    callback(null, newsArticles);
+                }
+
+              })
+          },
+          function(callback){
+            request("https://newsapi.org/v1/articles?source=business-insider&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+              if(!error&&response.statusCode){
+                  var responseObj = JSON.parse(body);
+                  var newsArticles = responseObj.articles;
+                  callback(null, newsArticles);
+              }
+
+            })
+          }, function(callback){
+              request("https://newsapi.org/v1/articles?source=cnbc&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+                if(!error&&response.statusCode){
+                    var responseObj = JSON.parse(body);
+                    var newsArticles = responseObj.articles;
+                    callback(null, newsArticles);
+                }
+
+              })
+          },function(callback){
+              request("https://newsapi.org/v1/articles?source=financial-times&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+                if(!error&&response.statusCode){
+                    var responseObj = JSON.parse(body);
+                    var newsArticles = responseObj.articles;
+                    callback(null, newsArticles);
+                }
+
+              })
+            },function(callback){
+                request("https://newsapi.org/v1/articles?source=the-wall-street-journal&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+                  if(!error&&response.statusCode){
+                      var responseObj = JSON.parse(body);
+                      var newsArticles = responseObj.articles;
+                      callback(null, newsArticles);
+                  }
+
+                })
+              },function(callback){
+                  request("https://newsapi.org/v1/articles?source=the-economist&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+                    if(!error&&response.statusCode){
+                        var responseObj = JSON.parse(body);
+                        var newsArticles = responseObj.articles;
+                        callback(null, newsArticles);
+                    }
+
+                  })
+                }
+        ], function(err, results){
+            if(err) {
+                console.log("error: "+ err);
+            }else{
+              getShuffledArticles(results, userId);
+            }
+
+        })
+    }
+    function getSportArticles(userId){
+
+          async.parallel([
+            function(callback){
+                request("https://newsapi.org/v1/articles?source=bbc-sport&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+                  if(!error&&response.statusCode){
+                      var responseObj = JSON.parse(body);
+                      var newsArticles = responseObj.articles;
+                      callback(null, newsArticles);
+                  }
+
+                })
+            },
+            function(callback){
+              request("https://newsapi.org/v1/articles?source=espn&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+                if(!error&&response.statusCode){
+                    var responseObj = JSON.parse(body);
+                    var newsArticles = responseObj.articles;
+                    callback(null, newsArticles);
+                }
+
+              })
+            }, function(callback){
+                request("https://newsapi.org/v1/articles?source=espn-cric-info&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+                  if(!error&&response.statusCode){
+                      var responseObj = JSON.parse(body);
+                      var newsArticles = responseObj.articles;
+                      callback(null, newsArticles);
+                  }
+
+                })
+            },function(callback){
+                request("https://newsapi.org/v1/articles?source=nfl-news&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+                  if(!error&&response.statusCode){
+                      var responseObj = JSON.parse(body);
+                      var newsArticles = responseObj.articles;
+                      callback(null, newsArticles);
+                  }
+
+                })
+              },function(callback){
+                  request("https://newsapi.org/v1/articles?source=sky-sports-news&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+                    if(!error&&response.statusCode){
+                        var responseObj = JSON.parse(body);
+                        var newsArticles = responseObj.articles;
+                        callback(null, newsArticles);
+                    }
+
+                  })
+                },function(callback){
+                    request("https://newsapi.org/v1/articles?source=fox-sports&apiKey=387b12d8c1e74fde941fbb27e7764398", function(error, response, body){
+                      if(!error&&response.statusCode){
+                          var responseObj = JSON.parse(body);
+                          var newsArticles = responseObj.articles;
+                          callback(null, newsArticles);
+                      }
+
+                    })
+                  }
+          ], function(err, results){
+              if(err) {
+                  console.log("error: "+ err);
+              }else{
+                getShuffledArticles(results, userId);
+              }
+
+          })
+      }
+
   function getShuffledArticles(results, userId){
       var outputArr = []
       results.forEach(function(element){
